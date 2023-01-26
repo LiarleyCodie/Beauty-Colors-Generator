9f55dd1d2725fbda46630d537755f178c317c21f
LiarleyCodie <liarley.personal@gmail.com>
2023-01-26 15:54:23 -0300

    State of mode buttons (navbar) change properties of the generator object

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const valueDisplayEl = document.querySelector("#valueDisplay")
 const newColorBtn = document.querySelector("#generatenew")
 const copyToClipboardBtn = document.querySelector("#copytoclipboard")
+const navButtons = document.querySelectorAll("nav button")
 
 const generate = {
+  methodsAllowed: {
+    hex: false, rgb: false, text: true
+  },
   hex: function () {
     const hexChars = "0123456789ABCDEF"
     const hexData = []
     for (let i = 0; i <= 6; i++) {
       hexData.push(hexChars[Math.floor(Math.random() * hexChars.length)])
     }
+    hexData.unshift("#")
     return hexData.join("")
   },
   rgb: function () {
@@ -16,13 +21,15 @@ const generate = {
     for (let i = 0; i < 3; i++) {
       rgbData.push(Math.floor(Math.random() * 255))
     }
-    return rgbData.join(", ")
+    rgbData.join(", ")
+    const codeFormat = `rgb(${rgbData})`
+    return codeFormat
   },
-  simple: function() {
+  simple: function () {
     const colors = ["red", "blue", "green", "brown", "chartreuse", "coral", "crimson", "darkorange", "orange", "orangered", "deeppink"]
     return colors[Math.floor([Math.random() * colors.length])]
   },
-  random: function() {
+  random: function () {
     const colorData = []
     const methods = [this.hex, this.rgb, this.simple]
     for (let i = 0; i < 3; i++) {
@@ -32,6 +39,28 @@ const generate = {
   }
 }
 
-newColorBtn.addEventListener("click", function() {
-  console.log(generate.random())
+const changeElements = {
+  background: function (value) {
+    document.body.style.backgroundColor = value
+  },
+  textElement: function (string) {
+    valueDisplayEl.innerText = string
+  }
+}
+
+newColorBtn.addEventListener("click", function () {
+  const value = generate.random()
+  changeElements.background(value)
+  changeElements.textElement(value)
+  console.log(value)
 })
+
+navButtons.forEach(button => button.addEventListener("click", function () {
+  if (generate.methodsAllowed[button.getAttribute("data-mode")] === false) {
+    generate.methodsAllowed[button.getAttribute("data-mode")] = true
+    button.classList.add("active")
+  } else {
+    generate.methodsAllowed[button.getAttribute("data-mode")] = false
+    button.classList.remove("active")
+  }
+}))
